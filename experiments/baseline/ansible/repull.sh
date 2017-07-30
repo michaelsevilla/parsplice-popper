40dd19d5a46bfeee99e4d51cc2ca81d0311d952f
Michael Sevilla <mikesevilla3@gmail.com>
2017-07-30 14:17:51 -0700

    deploy: port to piha (internal cluster)
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- experiments/baseline/ansible/repull.sh
+++ experiments/baseline/ansible/repull.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 set -ex
-LOGIN="docker login --password $DOCKER_PASSWORD --username mikesevilla3 registry.gitlab.com"
-DPULL="docker pull registry.gitlab.com/mikesevilla3/parsplice"
+DPULL="docker pull piha.soe.ucsc.edu:5000/parsplice"
 cd .. 
-time ./run.sh --forks 50  mpiclient,mpiserver -m shell -a "$LOGIN && $DPULL"
+time ./run.sh --forks 50  mpiclient,mpiserver -m shell -a "$DPULL"
